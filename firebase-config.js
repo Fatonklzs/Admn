a679581b24d2c393b5015b71d8862c08e1948e6e
Fatonklzs <ftnklsszzs@gmail.com>
2025-07-22 14:58:44 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,14 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBT_fE8bw5i3WQzL3D2FqtiRcMu2FcJ4",
-  authDomain: "putra-84.firebaseapp.com",
-  databaseURL: "https://putra-84-default-rtdb.firebaseio.com",
-  projectId: "putra-84",
-  storageBucket: "putra-84.firebasestorage.app",
-  messagingSenderId: "771337257712",
-  appId: "1:771337257712:web:12acd2fa7b0d7281f3b835",
-  measurementId: "G-NCYTKQPPC7"
+  apiKey: "AIzaSyB6Js3sXCV8B8qAb51dztNtR0IUbUCAmD8",
+  authDomain: "websi-3aad8.firebaseapp.com",
+  databaseURL: "https://websi-3aad8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "websi-3aad8",
+  storageBucket: "websi-3aad8.firebasestorage.app",
+  messagingSenderId: "92206097273",
+  appId: "1:92206097273:web:783ed1109d79786d20c5a8",
+  measurementId: "G-TM6P2LL17W"
 };
 
 // Initialize Firebase
